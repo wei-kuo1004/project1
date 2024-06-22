34879b63827d841e3b069c29373fa5476d996fbe
weikuo <wei-kuo@pcbut.com.tw>
2024-06-22 20:00:40 +0800

    Initial commit from Create Next App

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "UniTech",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/CinemaCafe/next-template",
+    docs: "https://ui.shadcn.com",
+  },
+}
