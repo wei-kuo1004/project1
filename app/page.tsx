b3cfc1a33a99932735f4fbe7cffe65d07d57c906
weikuo <wei-kuo@pcbut.com.tw>
2024-06-24 08:12:52 +0800

    (fix)Title &content

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,12 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          危害辨識 <br className="hidden sm:inline" />
-          Hazard Identification
+          首頁 <br className="hidden sm:inline" />
+          Home
         </h1>
         <p className="text-muted-foreground max-w-[700px] text-lg">
-          對於工作環境會有的危險，你認識多少？
+          用gh-page將github Page Run起來
         </p>
-        <p className="text-muted-foreground max-w-[700px] text-lg">How much do you know about potential hazards in the workplace?</p>
       </div>
       <div className="flex gap-4">
         <Link
